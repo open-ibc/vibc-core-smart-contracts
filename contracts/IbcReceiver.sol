a4225b589085dc58c14c901c49a8069422e4cf1e
WenweiX <wenwei@polymer>
2023-07-12 18:04:43 -0400

    migrate packet fee PR

--- contracts/IbcReceiver.sol
+++ contracts/IbcReceiver.sol
@@ -3,30 +3,7 @@
 pragma solidity ^0.8.9;
 
 import './IbcDispatcher.sol';
-
-struct IbcEndpoint {
-    string portId;
-    bytes32 channelId;
-}
-
-/// In IBC each package must set at least one type of timeout:
-/// the timestamp or the block height.
-struct IbcTimeout {
-    uint64 blockHeight;
-    uint64 timestamp;
-}
-
-struct IbcPacket {
-    /// identifies the channel and port on the sending chain.
-    IbcEndpoint src;
-    /// identifies the channel and port on the receiving chain.
-    IbcEndpoint dest;
-    /// The sequence number of the packet on the given channel
-    uint64 sequence;
-    bytes data;
-    /// when packet times out, measured on remote chain
-    IbcTimeout timeout;
-}
+import './Ibc.sol';
 
 /**
  * @title IbcReceiver
