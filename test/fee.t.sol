8a3e7e74a7e301ecc038cc39c85c77a30cd19842
WenweiX <wenwei@polymer>
2023-08-02 10:49:04 -0400

    create FeeTest contract for all packet fee related tests

---
+++ test/fee.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import 'forge-std/Test.sol';
+import '../contracts/Ibc.sol';
+import {Dispatcher, InitClientMsg, UpgradeClientMsg} from '../contracts/Dispatcher.sol';
+import {IbcReceiver} from '../contracts/IbcReceiver.sol';
+import '../contracts/IbcVerifier.sol';
+import '../contracts/Verifier.sol';
+import '../contracts/Mars.sol';
+import {ChannelOpenTestBase} from './Dispatcher.t.sol';
+
+contract FeeTest is ChannelOpenTestBase {
+    // default params
+    string payload = 'msgPayload';
+    uint64 timeoutTimestamp = 1000;
+    PacketFee fee = PacketFee(1 ether, 2 ether, 3 ether);
+
+    // pay extra packet fee after packet creation
+    function test_payPacketFeeAsync() public {
+        PacketFee memory accu = mergeFees(fee, fee);
+        for (uint64 index = 0; index < 3; index++) {
+            uint64 packetSeq = index + 1;
+
+            mars.greet{value: calcFee(fee)}(IbcDispatcher(dispatcher), payload, channelId, timeoutTimestamp, fee);
+            dispatcher.payPacketFeeAsync{value: calcFee(fee)}(address(mars), channelId, packetSeq, fee);
+
+            PacketFee memory packetFee = dispatcher.getTotalPacketFees(address(mars), channelId, packetSeq);
+            assertEq(keccak256(abi.encode(packetFee)), keccak256(abi.encode(accu)));
+        }
+    }
+
+    // sendPacket fails if insufficient fee is paid.
+    function test_insufficientFee() public {
+        vm.expectRevert();
+        mars.greet{value: calcFee(fee) - 1}(IbcDispatcher(dispatcher), payload, channelId, timeoutTimestamp, fee);
+    }
+}
