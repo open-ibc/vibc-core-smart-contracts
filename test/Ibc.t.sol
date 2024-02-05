5969d270b4954507daea8c3c2154aa86a629261e
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2024-02-05 17:48:31 +0100

    add ibc related util functions

---
+++ test/Ibc.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import '../contracts/Ibc.sol';
+import 'forge-std/Test.sol';
+
+contract IbcTest is Test {
+    function test_packet_commitment_proof_key() public {
+        IbcPacket memory packet = IbcPacket(
+            IbcEndpoint('portid', hex'6368616e6e656c2d30'),
+            IbcEndpoint('', 0),
+            12,
+            hex'',
+            Height(0, 0),
+            0
+        );
+        assertEq('commitments/ports/portid/channels/channel-0/sequences/12', Ibc.packetCommitmentProofKey(packet));
+    }
+
+    function test_packet_ack_proof_key() public {
+        IbcPacket memory packet = IbcPacket(
+            IbcEndpoint('', 0),
+            IbcEndpoint('portid', hex'6368616e6e656c2d30'),
+            12,
+            hex'',
+            Height(0, 0),
+            0
+        );
+        assertEq('acks/ports/portid/channels/channel-0/sequences/12', Ibc.ackProofKey(packet));
+    }
+
+    function test_channel_proof_key() public {
+        bytes memory key = bytes(
+            'channelEnds/ports/polyibc.eth1.71C95911E9a5D330f4D621842EC243EE1343292e/channels/channel-0'
+        );
+
+        assertEq(
+            key,
+            Ibc.channelProofKey('polyibc.eth1.71C95911E9a5D330f4D621842EC243EE1343292e', Ibc.toBytes32('channel-0'))
+        );
+    }
+
+    function test_string_to_bytes32() public {
+        assertEq(bytes32(hex'6368616e6e656c2d30'), Ibc.toBytes32('channel-0'));
+    }
+
+    function test_bytes32_to_string() public {
+        assertEq('channel-0', Ibc.toStr(bytes32(hex'6368616e6e656c2d30')));
+    }
+
+    function test_uint256_to_string() public {
+        assertEq('1', Ibc.toStr(1));
+        assertEq('112233445566', Ibc.toStr(112233445566));
+        assertEq('16', Ibc.toStr(16));
+    }
+
+    function test_string_to_uint256() public {
+        assertEq(1, Ibc.toUint('1'));
+        assertEq(112233445566, Ibc.toUint('112233445566'));
+        assertEq(16, Ibc.toUint('16'));
+    }
+
+    function test_parse_ack() public {
+        // this data is taken from the write_acknowledgement event emitted by polymer
+        bytes memory ack = bytes(
+            '{"result":"eyAiYWNjb3VudCI6ICJhY2NvdW50IiwgInJlcGx5IjogImdvdCB0aGUgbWVzc2FnZSIgfQ=="}'
+        );
+
+        AckPacket memory parsed = Ibc.parseAckData(ack);
+        assertTrue(parsed.success);
+        assertEq(bytes('{ "account": "account", "reply": "got the message" }'), parsed.data);
+
+        bytes memory error = bytes('{"error":"this is an error message"}');
+        AckPacket memory parsederr = Ibc.parseAckData(error);
+        assertFalse(parsederr.success);
+        assertEq(bytes('this is an error message'), parsederr.data);
+    }
+}
