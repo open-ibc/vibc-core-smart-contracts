b0e5f59fc87e750900abefb593e4e6d40662e9d5
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2024-02-07 12:32:07 +0100

    remove unused code

--- test/universal.channel.t.sol
+++ test/universal.channel.t.sol
@@ -2,12 +2,10 @@
 pragma solidity ^0.8.13;
 
 import '../contracts/Ibc.sol';
-import {Dispatcher, InitClientMsg, UpgradeClientMsg} from '../contracts/Dispatcher.sol';
+import {Dispatcher} from '../contracts/Dispatcher.sol';
 import {IbcEventsEmitter} from '../contracts/IbcDispatcher.sol';
 import {IbcReceiver} from '../contracts/IbcReceiver.sol';
-import '../contracts/IbcVerifier.sol';
 import '../contracts/UniversalChannelHandler.sol';
-import '../contracts/Verifier.sol';
 import '../contracts/Mars.sol';
 import '../contracts/OpConsensusStateManager.sol';
 import './Dispatcher.base.t.sol';
