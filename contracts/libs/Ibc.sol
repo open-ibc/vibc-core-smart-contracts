f72e7943dc945ba57b0c412077b6d4cf18629292
RnkSngh <raunaksingh2.718@gmail.com>
2024-03-21 02:25:52 -0400

    add UCH logging + new to/from UCHPacket encoding

--- contracts/libs/Ibc.sol
+++ contracts/libs/Ibc.sol
@@ -170,6 +170,27 @@ library IBCErrors {
 library IbcUtils {
     error StringTooLong();
 
+    // fromUniversalPacketBytes converts UniversalPacketDataBytes to UniversalPacketData, per how its packed into bytes
+    function fromUniversalPacketBytes(bytes calldata data)
+        external
+        pure
+        returns (UniversalPacket memory universalPacketData)
+    {
+        bytes32 srcPortAddr;
+        uint256 mwBitmap;
+        bytes32 destPortAddr;
+        assembly {
+            // Keep reusing 0x0 to move from calldata to return vars
+            calldatacopy(0x0, data.offset, 32)
+            srcPortAddr := mload(0x0)
+            calldatacopy(0x0, add(data.offset, 32), 32)
+            mwBitmap := mload(0x0)
+            calldatacopy(0x0, add(data.offset, 64), 32)
+            destPortAddr := mload(0x0)
+        }
+        universalPacketData = UniversalPacket(srcPortAddr, uint256(mwBitmap), destPortAddr, data[96:data.length]);
+    }
+
     /**
      * Convert a non-0x-prefixed hex string to an address
      * @param hexStr hex string to convert to address. Note that the hex string must not include a 0x prefix.
@@ -218,19 +239,8 @@ library IbcUtils {
         }
     }
 
-    // convert params to UniversalPacketBytes with optimal gas cost
-
     function toUniversalPacketBytes(UniversalPacket memory data) internal pure returns (bytes memory packetBytes) {
-        packetBytes = abi.encode(data);
-    }
-
-    // fromUniversalPacketBytes converts UniversalPacketDataBytes to UniversalPacketData, per how its packed into bytes
-    function fromUniversalPacketBytes(bytes memory data)
-        internal
-        pure
-        returns (UniversalPacket memory universalPacketData)
-    {
-        universalPacketData = abi.decode(data, (UniversalPacket));
+        packetBytes = bytes.concat(data.srcPortAddr, bytes32(data.mwBitmap), data.destPortAddr, data.appData);
     }
 
     // addressToPortId converts an address to a port ID
