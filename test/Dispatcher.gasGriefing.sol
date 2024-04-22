4b5cff621f986b2f2c236bc07607600a3e47596f
RnkSngh <raunaksingh2.718@gmail.com>
2024-04-22 15:15:42 -0400

    add gas griefing test

---
+++ test/Dispatcher.gasGriefing.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Base} from "./Dispatcher.Base.t.sol";
+import {GasUsingMars} from "./mocks/GasUsingMars.sol";
+import {IbcEndpoint, ChannelEnd, IbcUtils, IbcPacket} from "../contracts/libs/Ibc.sol";
+import {TestUtilsTest} from "./TestUtils.t.sol";
+
+contract DispatcherGasGriefing is Base {
+    IbcEndpoint src = IbcEndpoint("polyibc.bsc.58b604DB8886656695442374D8E940D814F2eDd4", "channel-99");
+    IbcEndpoint dest;
+    bytes payload = bytes("msgPayload");
+    bytes appAck = abi.encodePacked('{ "account": "account", "reply": "got the message" }');
+
+    GasUsingMars gasUsingMars;
+    ChannelEnd ch0 =
+        ChannelEnd("polyibc.eth.71C95911E9a5D330f4D621842EC243EE1343292e", IbcUtils.toBytes32("channel-0"), "1.0");
+    ChannelEnd ch1 =
+        ChannelEnd("polyibc.eth.71C95911E9a5D330f4D621842EC243EE1343292e", IbcUtils.toBytes32("channel-1"), "1.0");
+
+    function setUp() public override {
+        (dispatcherProxy, dispatcherImplementation) =
+            TestUtilsTest.deployDispatcherProxyAndImpl(portPrefix, dummyConsStateManager);
+        gasUsingMars = new GasUsingMars(2_000_000, dispatcherProxy);
+    }
+
+    function test_GasGriefing() public {
+        IbcPacket memory packet;
+        packet.data = bytes("packet-1");
+        packet.timeoutTimestamp = 15_566_401_733_896_437_760;
+        packet.dest.channelId = ch1.channelId;
+        packet.dest.portId = string(abi.encodePacked(portPrefix, IbcUtils.toHexStr(address(gasUsingMars))));
+        packet.src.portId = ch0.portId;
+        packet.src.channelId = ch0.channelId;
+        packet.sequence = 1;
+        dispatcherProxy.recvPacket{gas: 2_000_000}(packet, validProof);
+    }
+}
