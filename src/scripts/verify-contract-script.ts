1c541c4c77aef0bc7d1f2bbf515fcb4020a170eb
RnkSngh <raunaksingh2.718@gmail.com>
2024-07-22 20:35:16 -0400

    add verify script for verifying a whole spec file

--- src/scripts/verify-contract-script.ts
+++ src/scripts/verify-contract-script.ts
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 import {
+  ChainFolder,
+  parseObjFromFile,
   parseVerifyArgsFromCLI,
   readFromDeploymentFile,
   readMetadata,
 } from "../utils/io";
 import { $, cd } from "zx";
 import { MODULE_ROOT_PATH } from "../utils/constants";
-/**
- * Take a deployment name, and verify the result to blockscout api
- */
-async function main() {
-  const { deploymentName, verifierUrl, chainFolder, etherscanApiKey } =
-    await parseVerifyArgsFromCLI();
+import { Logger } from "winston";
+import { getMainLogger } from "../utils/cli";
+import { ContractItemSchema } from "../evm/schemas/contract";
+import { loadContractUpdateRegistry } from "../evm/schemas/contractUpdate";
 
+const verifyContract = async (
+  deploymentName: string,
+  chainFolder: ChainFolder,
+  etherscanApiKey: string,
+  verifierUrl: string,
+  logger: Logger
+) => {
   // Read deployment file, so that we can find path to artifact
   const deployment = await readFromDeploymentFile(deploymentName, chainFolder);
   const metadata = await readMetadata(deployment.factory);
@@ -27,7 +34,6 @@ async function main() {
     `${etherscanApiKey}`,
     `--verifier-url=${verifierUrl}`,
   ];
-
   // Find libraries into string that foundry verification expects if there are any
   let libraries: Record<string, string> | null = null;
   if (deployment.libraries && deployment.libraries.length > 0) {
@@ -37,7 +43,7 @@ async function main() {
     });
   }
 
-  console.log(
+  logger.info(
     `verifying ${deploymentName}'s deployment with ${deployment.factory} ${
       libraries ? `and libraries ${libraries}` : ``
     }`
@@ -49,10 +55,44 @@ async function main() {
   try {
     command = await $`forge verify-contract ${args} 2>&1 | tee verify-out.txt`; // command = await $`cd ${MODULE_ROOT_PATH} && forge verify-contract ${
   } catch (e) {
-    console.log("error ", e);
+    logger.error("error ", e);
   }
 
-  console.log("verification result ", command);
+  logger.info("verification result ", command);
+};
+/**
+ * Take a deployment name, and verify the result to blockscout api
+ */
+async function main() {
+  const { verifierUrl, chainFolder, etherscanApiKey, updateSpecs } =
+    await parseVerifyArgsFromCLI();
+
+  // Fetch spec from contract
+
+  const contractUpdates = loadContractUpdateRegistry(
+    parseObjFromFile(updateSpecs)
+  );
+
+  const logger = getMainLogger();
+  for (const contractUpdate of contractUpdates.values()) {
+    const parsed = ContractItemSchema.safeParse(contractUpdate);
+    if (parsed.success) {
+      // Only try to verify contractName if it matches the deploymentName
+      try {
+        await verifyContract(
+          parsed.data.name,
+          chainFolder,
+          etherscanApiKey,
+          verifierUrl,
+          logger
+        );
+      } catch (e) {
+        logger.error(
+          `Failed to verify contract ${parsed.data.name} with error: ${e}`
+        );
+      }
+    }
+  }
 }
 
 main();
