385aecffb80e8c57ff7f3f7d44246a35ebea82f1
RnkSngh <raunaksingh2.718@gmail.com>
2024-08-26 15:25:37 -0400

    fix tsup exports in npm package

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import { parseObjFromFile } from "./utils/io";
 import { loadEvmAccounts } from "./evm/schemas/account";
 import { deployToChain } from "./deploy";
 import { sendTxToChain } from "./tx";
+import { updateContractsForChain } from "./updateContract";
 
 export {
   deployToChain,
   sendTxToChain,
+ updateContractsForChain,
   Chain,
   Registry,
   loadEvmAccounts,
