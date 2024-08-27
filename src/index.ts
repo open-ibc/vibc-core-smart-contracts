48622c08157af58288990d69419d6f3b0699ee21
RnkSngh <raunaksingh2.718@gmail.com>
2024-08-27 03:40:25 -0400

    remove unused scripts + specs

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,9 @@ import { Registry } from "./utils/registry";
 import { ContractRegistryLoader } from "./evm/schemas/contract";
 import { parseObjFromFile } from "./utils/io";
 import { loadEvmAccounts } from "./evm/schemas/account";
-import { deployToChain } from "./deploy";
-import { sendTxToChain } from "./tx";
 import { updateContractsForChain } from "./updateContract";
 
 export {
-  deployToChain,
-  sendTxToChain,
  updateContractsForChain,
   Chain,
   Registry,
