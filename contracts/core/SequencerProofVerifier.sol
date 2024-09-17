9e38dc384526cd78adaf2474278f16d6f54bf231
RnkSngh <raunaksingh2.718@gmail.com>
2024-09-17 00:34:44 -0400

    add op lightclient/proofverifier for easy diff viewing

---
+++ contracts/core/SequencerProofVerifier.sol
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * Copyright 2024, Polymer Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+pragma solidity 0.8.15;
+
+import {SecureMerkleTrie} from "optimism/libraries/trie/SecureMerkleTrie.sol";
+import {RLPReader} from "optimism/libraries/rlp/RLPReader.sol";
+import {RLPWriter} from "optimism/libraries/rlp/RLPWriter.sol";
+import {IProofVerifier, L1Header, OpL2StateProof, Ics23Proof, OpIcs23Proof} from "../interfaces/IProofVerifier.sol";
+
+/**
+ * @title OptimisticProofVerifier
+ * @notice Verifies proofs related to Optimistic Rollup state updates
+ * @author Polymer Labs
+ */
+contract OptimisticProofVerifier is IProofVerifier {
+    using RLPReader for RLPReader.RLPItem;
+    using RLPReader for bytes;
+
+    // @notice index of the l1 state root in the "l1 header"
+    uint256 internal constant _L1_STATE_ROOT_INDEX = 3;
+
+    // @notice index of the l1 number in the "l1 header"
+    uint256 internal constant _L1_NUMBER_INDEX = 8;
+
+    // @notice known L2 Output Oracle contract address to verify state update proofs against
+    address public l2OutputOracleAddress;
+
+    constructor(address _l2OutputOracleAddress) {
+        l2OutputOracleAddress = _l2OutputOracleAddress;
+    }
+
+    /**
+     * @dev Prove that the provided app hash (L2 state root) is valid. Done so by proving that the L2OutputOracle
+     * contains an output proposal within its state that we can derive from the given app hash. The high level
+     * approach is:
+     *
+     * A. Prove the given L1 state root.
+     * B. Prove the prescence of an output proposal in the L2OutputOracle contract.
+     * C. Derive the output proposal from the apphash.
+     *
+     * A more detailed explanation of the process goes as follows. All steps must be valid in order for the
+     * app hash to be accepted. Otherwise, the function will revert.
+     *
+     * 1. Provided L1 header hash and number match that of the trusted ones. The trusted attributes must come
+     *    from Optimism's L1Block contract. Given that this contract only holds the latest L1 attributes, there's
+     *    a good chance for a race-condition to happen, so check them first.
+     *
+     * 2. Provided L1 header data includes the L1 state root. Compute the header hash and check it against the
+     *    trusted one. In case of a match, the state root must be valid.
+     *
+     * 3. Based on the L1 state root and using the provided account proof and L2OutputOracle address, get the
+     *    value stored in the MerkleTrie leaf. This is the state account.
+     *
+     * 4. With the state account root and using the provided storage proof and output proposal key, get the
+     *    vlue stored in the MerkleTrie leaf. This is the output proposal root.
+     *
+     * 5. With the provided apphash and L2 Blockc hash, try to compute a new output root and match it against
+     *    the one we just proved to be valid.
+     */
+    function verifyStateUpdate(
+        L1Header calldata l1header,
+        OpL2StateProof calldata proof,
+        bytes32 appHash,
+        bytes32 trustedL1BlockHash,
+        uint64 trustedL1BlockNumber
+    ) external view {
+        if (trustedL1BlockNumber != l1header.number) {
+            revert InvalidL1BlockNumber();
+        }
+
+        // This computes the L1 header hash
+        if (trustedL1BlockHash != keccak256(RLPWriter.writeList(l1header.header))) {
+            revert InvalidL1BlockHash();
+        }
+
+        // These two checks are here to verify that the "plain" (i.e. not RLP encoded) values in the l1header are
+        // the same ones found in l1header.header (i.e. RLP encoded). This is because it is cheaper to RLP
+        // encode that decode
+        if (keccak256(RLPWriter.writeUint(l1header.number)) != keccak256(l1header.header[_L1_NUMBER_INDEX])) {
+            revert InvalidRLPEncodedL1BlockNumber();
+        }
+        if (
+            keccak256(RLPWriter.writeBytes(abi.encode(l1header.stateRoot)))
+                != keccak256(l1header.header[_L1_STATE_ROOT_INDEX])
+        ) {
+            revert InvalidRLPEncodedL1StateRoot();
+        }
+
+        //  stateAccount looks like this struct. We are interested in the Root field which is the one at index 2
+        //  type StateAccount struct {
+        //     Nonce    uint64       // index 0
+        //     Balance  *big.Int     // index 1
+        //     Root     common.Hash  // index 2
+        //     CodeHash []byte       // index 3
+        //  }
+        RLPReader.RLPItem[] memory stateAccount = SecureMerkleTrie.get(
+            abi.encodePacked(l2OutputOracleAddress), proof.accountProof, l1header.stateRoot
+        ).toRLPItem().readList();
+
+        bytes memory outputRoot = SecureMerkleTrie.get(
+            abi.encode(proof.l2OutputProposalKey), proof.outputRootProof, bytes32(bytes(stateAccount[2].readBytes()))
+        );
+
+        // Now that the output root is verified, we need to verify the app hash. To do so we try to derive the
+        // the output root the same way the proposer did.
+        // See https://github.com/polymerdao/optimism/blob/polymer/v1.2.0/op-service/eth/output.go#L44
+        if (
+            keccak256(
+                abi.encodePacked(
+                    bytes32(0), // version
+                    appHash,
+                    bytes32(0), // message passer storage root.
+                    proof.l2BlockHash
+                )
+            ) != bytes32(bytes(outputRoot.toRLPItem().readBytes()))
+        ) {
+            revert InvalidAppHash();
+        }
+    }
+
+    /**
+     * @dev Prove that a given state is not part of a proof
+     * @dev this method is mainly used for packet timeouts, which is currently not implemented
+     */
+    function verifyNonMembership(bytes32, bytes calldata, Ics23Proof calldata) external pure {
+        revert MethodNotImplemented();
+    }
+
+    /**
+     * @inheritdoc IProofVerifier
+     * @dev verifies a chain of ICS23 proofs
+     * Each computed subroot starting from index 0 must match the value of the next proof (hence chained proofs).
+     * The cosmos SDK and ics23 support chained proofs to switch between different proof specs.
+     * Custom proof specs are not supported here. Only Iavl and Tendermint or similar proof specs are supported.
+     */
+    function verifyMembership(bytes32 appHash, bytes calldata key, bytes calldata value, Ics23Proof calldata proofs)
+        external
+        pure
+    {
+        // first check that the provided proof indeed proves the keys and values.
+        if (keccak256(key) != keccak256(proofs.proof[0].key)) {
+            revert InvalidProofKey();
+        }
+        if (keccak256(value) != keccak256(proofs.proof[0].value)) revert InvalidProofValue();
+        // proofs are chained backwards. First proof in the list (proof[0]) corresponds to the packet proof, meaning
+        // that can be checked against the next subroot value (i.e. ibc root). Once the first proof is verified,
+        // we can check the second that corresponds to the ibc proof, that is checked against the app hash (app root)
+        if (bytes32(proofs.proof[1].value) != _verify(proofs.proof[0])) revert InvalidPacketProof();
+        if (appHash != _verify(proofs.proof[1])) revert InvalidIbcStateProof();
+    }
+
+    /**
+     * @dev Verifies an ICS23 proof through the root hash based on the provided proof.
+     * @dev This code was adapted from the ICS23 membership verification found here:
+     * https://github.com/cosmos/ics23/blob/go/v0.10.0/go/ics23.go#L36
+     * @param proof The ICS23 proof to be verified.
+     * @return computed The computed root hash.
+     */
+    function _verify(OpIcs23Proof calldata proof) internal pure returns (bytes32 computed) {
+        bytes32 hashedData = sha256(proof.value);
+        computed = sha256(
+            abi.encodePacked(
+                proof.prefix, _encodeVarint(proof.key.length), proof.key, _encodeVarint(hashedData.length), hashedData
+            )
+        );
+
+        for (uint256 i = 0; i < proof.path.length; i++) {
+            computed = sha256(abi.encodePacked(proof.path[i].prefix, computed, proof.path[i].suffix));
+        }
+    }
+
+    /**
+     * @dev Encodes an integer value into a variable-length integer format.
+     * @param value The integer value to be encoded.
+     * @return encoded The encoded bytes array.
+     */
+    function _encodeVarint(uint256 value) internal pure returns (bytes memory encoded) {
+        bytes memory result;
+        while (value >= 0x80) {
+            bytes.concat(result, bytes1(uint8((value & 0x7F) | 0x80)));
+            value >>= 7;
+        }
+        encoded = bytes.concat(result, bytes1(uint8(value)));
+    }
+}
---
+++ contracts/core/SequencerSoloClient.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * Copyright 2024, Polymer Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+pragma solidity 0.8.15;
+
+import {L1Header, IProofVerifier, OpL2StateProof, Ics23Proof} from "../interfaces/IProofVerifier.sol";
+import {ILightClient} from "../interfaces/ILightClient.sol";
+import {L1Block} from "optimism/L2/L1Block.sol";
+
+/**
+ * @title OptimisticLightClient
+ * @author Polymer Labs
+ * @dev This specific light client implementation uses the same client that is used in the op-stack
+ */
+contract OptimisticLightClient is ILightClient {
+    // consensusStates maps from the height to the appHash.
+    mapping(uint256 => uint256) public consensusStates;
+
+    // fraudProofEndtime maps from the appHash to the fraud proof end time.
+    mapping(uint256 => uint256) public fraudProofEndtime;
+    uint256 public fraudProofWindowSeconds;
+    IProofVerifier public verifier;
+    L1Block public l1BlockProvider;
+
+    error CannotUpdatePendingOptimisticConsensusState();
+    error AppHashHasNotPassedFraudProofWindow();
+
+    constructor(uint32 fraudProofWindowSeconds_, IProofVerifier verifier_, L1Block _l1BlockProvider) {
+        fraudProofWindowSeconds = fraudProofWindowSeconds_;
+        verifier = verifier_;
+        l1BlockProvider = _l1BlockProvider;
+    }
+
+    /**
+     * @inheritdoc ILightClient
+     */
+    function updateClient(
+        L1Header calldata l1header,
+        bytes calldata encodedProof,
+        uint256 height,
+        uint256 appHash
+    ) external override returns (uint256 fraudProofEndTime, bool ended) {
+        OpL2StateProof memory proof = abi.decode(encodedProof, (OpL2StateProof));
+        uint256 hash = consensusStates[height];
+        if (hash == 0) {
+            // if this is a new apphash we need to verify the provided proof. This method will revert in case
+            // of invalid proof.
+            verifier.verifyStateUpdate(
+                l1header, proof, bytes32(appHash), l1BlockProvider.hash(), l1BlockProvider.number()
+            );
+
+            // a new appHash
+            consensusStates[height] = appHash;
+            uint256 endTime = block.timestamp + fraudProofWindowSeconds;
+            fraudProofEndtime[appHash] = endTime;
+            return (endTime, false);
+        }
+
+        if (hash == appHash) {
+            uint256 endTime = fraudProofEndtime[hash];
+            return (endTime, block.timestamp >= endTime);
+        }
+
+        revert CannotUpdatePendingOptimisticConsensusState();
+    }
+
+    /**
+     * @inheritdoc ILightClient
+     */
+    function getState(uint256 height) external view returns (uint256 appHash, uint256 fraudProofEndTime, bool ended) {
+        return getInternalState(height);
+    }
+    /**
+     * @inheritdoc ILightClient
+     */
+
+    function verifyMembership(Ics23Proof calldata proof, bytes calldata key, bytes calldata expectedValue)
+        external
+        view
+    {
+        // a proof generated at height H can only be verified against state root (app hash) from block H - 1.
+        // this means the relayer must have updated the contract with the app hash from the previous block and
+        // that is why we use proof.height - 1 here.
+        (uint256 appHash,, bool ended) = getInternalState(proof.height - 1);
+        if (!ended) revert AppHashHasNotPassedFraudProofWindow();
+        verifier.verifyMembership(bytes32(appHash), key, expectedValue, proof);
+    }
+
+    /**
+     * @inheritdoc ILightClient
+     */
+    function verifyNonMembership(Ics23Proof calldata proof, bytes calldata key) external view {
+        (uint256 appHash,, bool ended) = getInternalState(proof.height - 1);
+        if (!ended) revert AppHashHasNotPassedFraudProofWindow();
+        verifier.verifyNonMembership(bytes32(appHash), key, proof);
+    }
+
+    /**
+     * @inheritdoc ILightClient
+     */
+    function getFraudProofEndtime(uint256 height) external view returns (uint256 fraudProofEndTime) {
+        uint256 hash = consensusStates[height];
+        return fraudProofEndtime[hash];
+    }
+
+    /**
+     * @dev Returns the internal state of the light client at a given height.
+     */
+    function getInternalState(uint256 height)
+        public
+        view
+        returns (uint256 appHash, uint256 fraudProofEndTime, bool ended)
+    {
+        uint256 hash = consensusStates[height];
+        return (hash, fraudProofEndtime[hash], hash != 0 && block.timestamp >= fraudProofEndtime[hash]);
+    }
+}
